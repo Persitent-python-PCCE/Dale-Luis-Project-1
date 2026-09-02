4442640595ba3bf23a4298355e8783b7ceec3936
DaleLuis <dalerioluis@gmail.com>
2026-09-02 13:08:34 +0530

    Fixes

--- conftest.py
+++ conftest.py
@@ -4,30 +4,40 @@ import pytest
 
 # Ensure workspace root is in sys.path
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
 from app import create_app
 from config.database import db
 
+
 @pytest.fixture(scope="function")
 def app():
-    app = create_app({
+
+    test_config = {
         "TESTING": True,
         "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
-        "RATELIMIT_ENABLED": False
-    })
+        "RATELIMIT_ENABLED": False,
+    }
+
+    app = create_app(test_config)
 
     with app.app_context():
         db.session.expire_on_commit = False
+
         db.create_all()
+
         yield app
+
         db.session.remove()
         db.drop_all()
+
 
 @pytest.fixture(scope="function")
 def client(app):
     return app.test_client()
+
 
 @pytest.fixture(scope="function")
 def db_session(app):
